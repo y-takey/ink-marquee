dca239577d12fb8a1cb8f5c5f8f39e64ece3a0fa
takeyuichi <y.takey@gmail.com>
2017-07-16 15:09:03 +0900

    build run environment

---
+++ example/basic.js
@@ -0,0 +1,31 @@
+const {h, render, Component, Text} = require('ink');
+
+class Counter extends Component {
+  constructor() {
+    super();
+
+    this.state = { i: 0 };
+ }
+
+  render() {
+    return (
+      <Text green>
+        {this.state.i} tests passed
+      </Text>
+    );
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({
+        i: this.state.i + 1
+      });
+    }, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+}
+
+render(<Counter/>);
